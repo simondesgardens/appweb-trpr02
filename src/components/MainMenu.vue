f3f27cb8ae2c8659a143e6469d2c25a5de2796fd
Ilann Brodl <88870552+TheIlann74@users.noreply.github.com>
2024-04-16 13:50:10 -0400

    Le formulaire envoie les bonnes valeurs vers la route du jeu

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,34 +1,35 @@
 <script setup lang="ts">
 import { useRouter, onBeforeRouteLeave } from 'vue-router'
+import { ref } from 'vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
 const router = useRouter()
+let name = ref('default')
+let ship = ref('default')
 
-function startGame() {
-  router.push({ name: 'Mission', params: { name: 'YourName', ship: 'YourShip' } })
-}
+
 </script>
 
 
 <template>
     <h1>Votre objectif: survivre a 5 missions en obtenant le plus de credits galactiques.</h1>
     <form>
-  <fieldset>
-    <div class="mb-3">
-      <label for="textInput" class="form-label">Votre nom:</label>
-      <input type="text" id="disabledTextInput" class="form-control">
-    </div>
-    <div class="mb-3">
-      <label for="select" class="form-label">Votre vaisseau:</label>
-      <select id="select" class="form-select">
-        <option>Test</option>
-        <option>Test</option>
-      </select>
-    </div>
-    <button type="submit" class="btn btn-primary" @click="startGame()">Debuter la partie</button>
-  </fieldset>
-</form>
+      <fieldset>
+        <div class="mb-3">
+          <label for="textInput" class="form-label">Votre nom:</label>
+          <input type="text" id="textInput" class="form-control" v-model="name">
+        </div>
+        <div class="mb-3">
+          <label for="select" class="form-label">Votre vaisseau:</label>
+         <select id="select" class="form-select" v-model="ship">
+            <option value="default">Test</option>
+            <option>Test2</option>
+          </select>
+        </div>
+       <router-link :to="{ name: 'Mission', params: { name: name, ship: ship } }" tag="button" class="btn btn-primary">Debuter la partie</router-link>
+     </fieldset>
+    </form>
 </template>
 
 <style scoped>
