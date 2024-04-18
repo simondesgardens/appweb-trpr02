7a9070d2513da90a9b6bf5727a57473d4d14681c
Ilann Brodl <88870552+TheIlann74@users.noreply.github.com>
2024-04-18 15:38:58 -0400

    Ajout du tableau de score avec la bd

--- src/views/Score.vue
+++ src/views/Score.vue
@@ -1,11 +1,31 @@
 <script setup lang="ts">
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import DatabaseService from "../scripts/databaseService.ts"
+import { onMounted, ref } from 'vue'
+
+const databaseService = new DatabaseService();
+
+const scores = ref([]);
+
+onMounted(async () => {
+    scores.value = await databaseService.getRanking();
+    scores.value = putScoresInOrder(scores.value);
+});
+
+function putScoresInOrder(scores: any) {
+    return scores.sort((a: any, b: any) => b.score - a.score);
+}
 
 </script>
 
-
 <template>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item title text-center fw-bold">Pointage</li>
+        <li class="list-group-item" v-for="score in scores" :key="score.id" :value="score.name">{{ score.name }} - {{ score.score }} CG</li>
+    </ul>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
