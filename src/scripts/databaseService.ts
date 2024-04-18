d2f70ccab0e0e8d2b026171b55ade5cbecf58e81
simondesgardens <sim5@live.ca>
2024-04-18 16:14:22 -0400

    Ajout de postScore dans le databaseService.ts

--- src/scripts/databaseService.ts
+++ src/scripts/databaseService.ts
@@ -1,4 +1,4 @@
-import axios from 'axios' 
+import axios, { AxiosRequestConfig, AxiosResponse, RawAxiosRequestHeaders } from 'axios' 
 const RANKING_PATH = '/ranking'
 const SHIPS_PATH = '/ships'
 const CHARACTERS_PATH = '/characters'
@@ -21,5 +21,20 @@ export default class DatabaseService {
   async getCharacters () {
     const { data } = await axios.get(this.API_URL + CHARACTERS_PATH)
     return data;
+  }
+
+  async postScore(name: String, score: number) {
+    const config: AxiosRequestConfig = {
+      headers: {
+        'Accept': 'application/json',
+      } as RawAxiosRequestHeaders,
+    }
+
+    const id: number = (await axios.get(this.API_URL + RANKING_PATH + '?_page=1&_limit=1')).headers['x-total-count']
+
+    const data = {'id': id, 'name': name, 'score': score}
+    const response: AxiosResponse = await axios.post(this.API_URL + RANKING_PATH, data, config)
+    console.log(response.status)
+    console.log(response.data.json)
   }
 }
