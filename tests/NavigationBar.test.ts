afeb831c0a8f4416525a4c281b8aec0fe6faa222
TheIlann74 <Ilannilannbrodl@outlook.fr>
2024-04-22 23:26:31 -0400

    Modification des tests

--- tests/NavigationBar.test.ts
+++ tests/NavigationBar.test.ts
@@ -11,6 +11,25 @@ const router = createRouter({
 
 describe('NavigationBar.vue', () => {
 
+    it('Doit pouvoir naviguer sur la page du menu.', async () => {
+        router.push('/')
+        await router.isReady()
+    
+        const wrapper = mount(NavigationBar, {
+          global: {
+            plugins: [router]
+          }
+        })
+    
+        const routerSpy = vi.spyOn(router, 'push')
+    
+        const linkAboutEl = wrapper.find('#menu')
+        await linkAboutEl.trigger('click')
+    
+        expect(routerSpy).toHaveBeenCalledWith('/')
+      })
+    
+    
   it('Doit pouvoir naviguer sur la page des scores.', async () => {
     router.push('/')
     await router.isReady()
@@ -27,23 +46,5 @@ describe('NavigationBar.vue', () => {
     await linkPostsEl.trigger('click')
 
     expect(routerSpy).toHaveBeenCalledWith({ name: 'Score' })
-  })
-
-  it('Doit pouvoir naviguer sur la page du menu.', async () => {
-    router.push('/')
-    await router.isReady()
-
-    const wrapper = mount(NavigationBar, {
-      global: {
-        plugins: [router]
-      }
-    })
-
-    const routerSpy = vi.spyOn(router, 'push')
-
-    const linkAboutEl = wrapper.find('#menu') // Adapter le sélecteur selon ton besoin
-    await linkAboutEl.trigger('click')
-
-    expect(routerSpy).toHaveBeenCalledWith('/')
   })
 })
