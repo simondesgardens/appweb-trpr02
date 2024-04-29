e420338da78256d35cd566e7a63c0408cb12be45
simondesgardens <sim5@live.ca>
2024-04-28 23:27:36 -0400

    Ajout du workflows

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,25 +4,11 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "TP2 revue de code",
   description: "Revue de code pour le tp02",
+  base: '/appweb-trpr02/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { text: 'Home', link: '/' }
     ]
   }
 })
